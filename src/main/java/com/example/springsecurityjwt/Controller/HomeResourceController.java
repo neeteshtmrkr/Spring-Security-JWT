fefc775f6a5eeefafab81399a4344f7689a55921
tamrakarnitesh21 <tamrakarnitesh21@gmail.com>
2021-06-27 20:26:18 +0545

    Added the jjwt dependencu and jaxb api

--- src/main/java/com/example/springsecurityjwt/Controller/HomeResourceController.java
+++ src/main/java/com/example/springsecurityjwt/Controller/HomeResourceController.java
@@ -1,13 +1,15 @@
 package com.example.springsecurityjwt.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeResourceController {
 
-    @RequestMapping("/hello")
+    @RequestMapping({"/hello"})
     public String hello(){
-        return "<h2>Hello World!!</h2>";
+        return "<h2>Hello User, Welcome to the application!!</h2>";
     }
 }
